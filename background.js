75bd27cecaa80e2162cf44747415e00d8f93a6dd
Joel Barciauskas <jbarciauskas@bluestatedigital.com>
2013-07-29 18:57:27 -0400

    Add tabId as an index for the buttons state

--- background.js
+++ background.js
@@ -1,26 +1,29 @@
-var button = {
-  'active': false,
-  'status': null,
-  'hits':   null
-};
+var buttons = [];
 
 chrome.webRequest.onHeadersReceived.addListener(function (details) {
+  if(!(details.tabId in buttons)) {
+    buttons[details.tabId] = {
+      'active': false,
+      'status': null,
+      'hits':   null
+    };
+  }
   if (details.type === 'main_frame') {
     var headers = details.responseHeaders;
     for (var i = 0; i < headers.length; i++) {
       var header = headers[i];
       if (header.name === 'Via' && header.value.indexOf('varnish')) {
-        button.active = true;
+        buttons[details.tabId].active = true;
       }
       if (header.name === 'X-Cache') {
         if (header.value.indexOf('HIT') !== -1) {
-            button.status = 'hit';
+            buttons[details.tabId].status = 'hit';
         } else if (header.value.indexOf('MISS') !== -1) {
-            button.status = 'miss';
+            buttons[details.tabId].status = 'miss';
         }
       }
       if(header.name === 'X-Cache-Hits') {
-        button.hits =  header.value;
+        buttons[details.tabId].hits =  header.value;
       }
     }
   }
@@ -33,9 +36,9 @@ chrome.webRequest.onHeadersReceived.addListener(function (details) {
 
 chrome.webNavigation.onCompleted.addListener(function(details) {
   if (details.frameId === 0) {
-    var color = (button.active) ? 'blue' : 'gray';
-    if(button.status !== null) {
-      switch (button.status) {
+    var color = (buttons[details.tabId].active) ? 'blue' : 'gray';
+    if(buttons[details.tabId].status !== null) {
+      switch (buttons[details.tabId].status) {
         case 'hit':
           color = 'green';
           chrome.browserAction.setBadgeBackgroundColor({
@@ -43,7 +46,7 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
             tabId: details.tabId
           });
           chrome.browserAction.setBadgeText({
-            text: button.hits,
+            text: buttons[details.tabId].hits,
             tabId: details.tabId
           });
           break;
@@ -63,6 +66,10 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
     else {
         chrome.browserAction.setBadgeText({
         text: '',
+        tabId: details.tabId
+        });
+        chrome.browserAction.setBadgeBackgroundColor({
+        color: [0, 0, 0, 0],
         tabId: details.tabId
         });
     }
